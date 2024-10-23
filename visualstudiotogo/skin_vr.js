a6d1e447105a0390d86fe6eed57917d28a4ba9d4
Landozi Ziaad <57369614+ziaad26@users.noreply.github.com>
2024-10-23 09:57:32 +0000

    update

--- visualstudiotogo/skin_vr.js
+++ visualstudiotogo/skin_vr.js
@@ -1,7 +1,7 @@
 // Garden Gnome Software - VR - Skin
 // Pano2VR 7.1.2/20913
 // Filename: feather_vr.ggsk
-// Generated 2024-10-02T01:50:19
+// Generated 2024-10-23T09:54:01
 
 function pano2vrVrSkin(player,base) {
 	player.addVariable('node_cloner_vr_hasUp', 2, false, { ignoreInState: 0  });
