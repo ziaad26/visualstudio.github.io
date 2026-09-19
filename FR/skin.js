0f966124ab08a870fb70f55ea41c40a3692c64df
Landozi Ziaad <vzstudio26@gmail.com>
2026-09-19 21:30:37 +0000

    update

--- FR/skin.js
+++ FR/skin.js
@@ -1,7 +1,7 @@
 // Garden Gnome Software - Skin
 // Pano2VR 8.0.2/22555
 // Filename: Heritage.ggsk
-// Generated 2026-09-19T20:59:59Z
+// Generated 2026-09-19T21:29:41Z
 
 function pano2vrSkin(player,base) {
 	player.addVariable('vis_sounds_splashscreen', 2, false, { ignoreInState: 1  });
